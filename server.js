79358afa90888912caf03b60f5a73e65ca1fdd38
Pierre-LouisTALBOT <Pierre-LouisTALBOT@users.noreply.github.com>
2017-04-12 09:15:40 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+**
+ * Created by charles on 09/01/2017.
+ */
+
+/**
+ * External Module dependencies.
+ */
+const app = require('express')();
+const server = require('http').createServer(app);
+
+/**
+ * Express Route configuration
+ */
+app.get('/', function (req, res, next) {
+    res.sendFile(__dirname + 'index.html');
+});
+
+/**
+ * Start listen with the server
+ */
+const port = process.env.PORT || 8080;
+server.listen(port, () => console.log('Express server listening on %d, in %s mode', port, app.get('env')));
