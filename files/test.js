0bff34cdd9c18825a2c3b6eeb38f48fe10f4a3a3
Pierre-LouisTALBOT <pierrelouistalbot@gmail.com>
2018-02-01 16:29:52 +0100

    fi css link

--- files/test.js
+++ files/test.js
@@ -4,8 +4,7 @@ function changeStyleSheet(sheet) {
 
 function articleToPdf2() {
     //  /_layouts/15/skema.internet.2016/site/branding/css/skema-internet-pdf.css
-	//  https://rawgit.com/Pierre-LouisTALBOT/downloadSite/master/files/test.css
-    changeStyleSheet("https://raw.githubusercontent.com/Pierre-LouisTALBOT/downloadSite/master/files/test.css");
+    changeStyleSheet("https://rawgit.com/Pierre-LouisTALBOT/downloadSite/master/files/test.css");
     setTimeout(function () {
         window.print();
     }, 1000);
