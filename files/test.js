aaf253088fefc32442014a9d3c337f3b68332a1d
Pierre-LouisTALBOT <pierrelouistalbot@gmail.com>
2018-02-01 14:00:01 +0100

    test

--- files/test.js
+++ files/test.js
@@ -12,8 +12,8 @@ function changeStyleSheet(sheet) {
 function articleToPdf2() {
     changeStyleSheet("/_layouts/15/skema.internet.2016/site/branding/css/skema-internet-pdf.css");
     console.log("ok1");
-    vprint = setTimeout(printWindow(), 2000);
-    console.log("ok2");
+    //vprint = setTimeout(printWindow(), 2000);
+    //console.log("ok2");
     changeStyle = setTimeout(changeStyleSheet("null"), 7000);
     console.log("ok3");
 }
